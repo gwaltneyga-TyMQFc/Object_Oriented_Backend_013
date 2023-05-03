abc7eb52f36ebefdd7b77c1924731f72b5b8b85e
Gregory A. Gwaltney <gwaltneyga@gmail.com>
2023-05-03 16:34:22 -0400

    Updated all models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,19 +1,25 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: "product_tag",
   }
 );
 
